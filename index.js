6613cb99fe53d3a0c1a5bb255f872b38e1b451ea
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-28 13:18:41 +0530

    register route and login route added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //imports
 const express = require("express")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv").config()
 const app = express();
+const User = require("./models/User")
 
 // static files
 
@@ -10,6 +13,9 @@ app.use("/css", express.static(__dirname + "public/css"))
 app.use("/js", express.static(__dirname + "public/js"))
 app.use("/img", express.static(__dirname + "public/img"))
 app.use("/scss", express.static(__dirname + "public/scss"))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //set View engine
 app.set("views", "./views")
@@ -27,12 +33,49 @@ app.get("/login", (req, res) => {
     res.render("login")
 })
 
+//login post
+app.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email, password: req.body.password })
+        if (!user) {
+            res.status(401).json("user not found")
+        } else {
+
+            res.status(201).json("succesfully login")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
+
 //register ROute
 app.get("/register", (req, res) => {
     res.render("register")
 })
 
-mongoose.connect()
+app.post("/register", async (req, res) => {
+
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser)
+    } catch (err) {
+        res.status(401).json(err)
+    }
+})
+
+//mongoose connection
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("DB connected"))
+    .catch((err) => console.log(err))
 
 //listen
 const port = 4000;
--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
     password: { type: String, required: true },
 
 })
